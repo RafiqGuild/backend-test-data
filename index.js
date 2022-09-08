40016da20ace6ee1e1f3440538ea994fc6b2209d
Rafiq <rafiq.premji@guildeducation.com>
2022-09-08 14:54:53 -0600

    start

---
+++ index.js
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker';
+
+console.log(faker.name.fullName())
+
+// 12 majors in csv (make by hand)
+// id, name, major credits, min gpa
+
+const majorNames = [
+    ['Communications'],
+    ['Mathematics'],
+    ['Applied Mathematics'],
+    ['Computer Science'],
+    ['Physics'],
+    ['Marketing'],
+    ['Anatomy'],
+    [''],
+    [''],
+    [''],
+    [''],
+    [''],
+]
+
+// 120 course names (no csv)
+
+// generate 50k students (id], name)
+
+// generate 1M enrollments 
